199d2a3e0d19495735d04dd0a9337bb1800f270a
JohannCastillo <johannjco15022@gmail.com>
2024-07-24 18:59:19 -0500

    debug para saber qué pasa xd

--- ApiAnemia/settings.py
+++ ApiAnemia/settings.py
@@ -33,7 +33,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = not PRODUCTION
+DEBUG = True
 
 ALLOWED_HOSTS = ["https://apianemia.onrender.com", 'localhost', '127.0.0.1']
 
