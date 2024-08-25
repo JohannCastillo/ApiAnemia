e2a1f9d1ca15d4dccb137c7563cfcc68fc997faf
Parms221 <willy_g29@outlook.com>
2024-08-25 16:21:35 +0000

    remove unsused views

--- models/urls.py
+++ models/urls.py
@@ -1,11 +1,9 @@
 from django.urls import path
-from .views import pronostico, diagnostico, dieta, gpt
+from .views import pronostico, diagnostico, dieta
 from django.urls import path, include
 
 urlpatterns = [
     path('pronostico', view=pronostico.index),
     path('diagnostico', view=diagnostico.index),
     path('dieta', view=dieta.index),    
-    path('gpt/dieta/<int:id>', view=gpt.gpt_dieta),    
-    path('gpt/dietav2/<int:id>', view=gpt.gpt_dietav2),    
 ]
